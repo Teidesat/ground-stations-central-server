9892dd665dfd1ac61dee3b47508f701bef5accba
jdrguez <josedomingo.rguez.rguez@gmail.com>
2025-03-27 21:14:43 +0000

    Se añade Modelo y Serializador de SatelliteDAta

--- apps/dataflow/models.py
+++ apps/dataflow/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class SatelliteData(models.Model):
+    CATEGORY_CHOICES = [
+        ("TEMP", "temperature"),
+        ("POWR", "power"),
+        ("HUMI", "humidity"),
+        ("POSI", "position"),
+        ("GENE", "general"),
+    ] 
+
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES.GENE)
+    content = models.JSONField()
+    timestamp = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.timestamp} - {self.category} ({self.id})"
