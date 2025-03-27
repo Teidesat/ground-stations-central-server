4080f516cccae529213f26382753f0b4ab282c80
jdrguez <josedomingo.rguez.rguez@gmail.com>
2025-03-27 21:06:12 +0000

    Se añade el panel de admin sobre LogEntry

--- apps/logvault/admin.py
+++ apps/logvault/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import LogEntry
 
-# Register your models here.
+@admin.register(LogEntry)
+class LogEntry(admin.ModelAdmin):
+    list_display = (
+        'timestamp',
+        'level',
+        'logger',
+        'module',
+        'function',
+        'message',
+        'request_method',
+        'request_path',
+        'request_status_code',
+        'request_client_ip',
+        'request_user',
+        'exception_type',
+        'exception_message',
+        'exception_stack_trace'
+    )
+
--- apps/logvault/api.py
+++ apps/logvault/api.py
@@ -1,4 +1,18 @@
 from ninja import Router
+from .serializer import LogSerializer
+from .models import LogEntry
+from django.http import JsonResponse
+from .schema import LogSchema
 router = Router()
 
 
+@router.get('/')
+def all_logs(request):
+    try:
+        logs = LogEntry.objects.all()
+        logs_json = LogSerializer(logs, request=request)
+        return logs_json.json_response()
+    except LogEntry.DoesNotExist:
+        return JsonResponse({'error': 'No logs available'}, status=404)
+    except:
+        return JsonResponse({'error': 'Error interno'}, status=500)
