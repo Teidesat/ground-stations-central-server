f96857a32e00b7b1b691f1d7094c8140e5db1f53
DavidRiccio <dricciodeabreu1@gmail.com>
2025-04-10 18:34:14 +0100

    Se arregla el registro extra en el test

--- apps/dataflow/models.py
+++ apps/dataflow/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 
+
 class SatelliteData(models.Model):
     class CATEGORY_CHOICES(models.TextChoices):
-        TEMPERATURE = "TEMP", "temperature"
-        POWER = "POWR", "power"
-        HUMIDITY = "HUMI", "humidity"
-        POSITION = "POSI", "position"
-        GENERAL = "GENE", "general"
+        TEMPERATURE = 'TEMP', 'temperature'
+        POWER = 'POWR', 'power'
+        HUMIDITY = 'HUMI', 'humidity'
+        POSITION = 'POSI', 'position'
+        GENERAL = 'GENE', 'general'
 
-    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES.GENERAL)
+    category = models.CharField(
+        max_length=50, choices=CATEGORY_CHOICES, default=CATEGORY_CHOICES.GENERAL
+    )
     content = models.JSONField(blank=True, null=True)
     timestamp = models.DateTimeField(blank=True, null=True)
     raw_data = models.TextField(default='')
 
     def __str__(self):
-        return f"{self.timestamp} - {self.category} ({self.id})"
+        return f'{self.timestamp} - {self.category} ({self.id})'
